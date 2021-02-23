5753878ba3d1b8332272133182b9d6c8cdfdb1a7
dzaki <dzaki@qtrust.id>
2021-02-23 21:46:37 +0700

    edit message

--- config/validation.go
+++ config/validation.go
@@ -1,7 +1,11 @@
 package config
 
 import (
+    "log"
+    "github.com/go-playground/locales/id"
 	"github.com/go-playground/validator/v10"
+    "github.com/go-playground/universal-translator"
+    id_translate "github.com/go-playground/validator/v10/translations/id"
 )
 
 type ErrorResponse struct {
@@ -10,12 +14,34 @@ type ErrorResponse struct {
 
 
 func ValidateStruct(entity interface{}) []string {
+
     var errors []string
+
+    translate := id.New()
+    universal := ut.New(translate,translate)
+    trans,notfound := universal.GetTranslator("id")
+
+    if !notfound {
+        log.Fatal("translator ora di temukno. ceken maneh")
+    }
+
     validate := validator.New()
+
+    if errorValidate := id_translate.RegisterDefaultTranslations(validate,trans); errorValidate != nil {
+        log.Fatal(errorValidate)
+    }
+
+    _ = validate.RegisterTranslation("required", trans, func(universalTranslate ut.Translator) error {
+		return universalTranslate.Add("required", "{0} dikandani required kok pancet ngeyel kosong ae", true)
+	}, func(universalTranslate ut.Translator, errorField validator.FieldError) string {
+		result, _ := universalTranslate.T("required", errorField.Field())
+		return result
+	})
+
     err := validate.Struct(entity)
     if err != nil {
         for _, err := range err.(validator.ValidationErrors) {
-            errors = append(errors,err.Error())
+            errors = append(errors,err.Translate(trans))
         }
     }
     return errors
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module playgofiber
 go 1.15
 
 require (
+	github.com/go-playground/locales v0.13.0
+	github.com/go-playground/universal-translator v0.17.0
 	github.com/go-playground/validator/v10 v10.4.1
 	github.com/gofiber/fiber/v2 v2.5.0
 	github.com/joho/godotenv v1.3.0
